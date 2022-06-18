c4265105d1ef2291ea2adc780cda5c9e09064b48
Luis Eraso <luiseraso@gmail.com>
2022-06-18 16:52:03 -0500

    Add basci callback example

--- asincronismo/src/callbacks/index.js
+++ asincronismo/src/callbacks/index.js
@@ -1,2 +1,25 @@
 console.log("============================callbacks/index.js============================")
 
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+function calc(num1, num2, callback) {
+  return callback(num1, num2);
+}
+
+function date(callback) {
+    console.log(new Date);
+    setTimeout(() => {
+        let date = new Date;
+        callback(date);
+    }, 3000);
+}
+
+function printDate(pDate) {
+    console.log(pDate);
+}
+
+console.log(calc(2, 3, sum));
+date(printDate);
+
