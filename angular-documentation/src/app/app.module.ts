6fecf787c562c51153cc23a591ca1a930434f26b
Luis Eraso <luiseraso@gmail.com>
2022-11-09 18:32:17 -0500

    Add CartService

--- angular-documentation/src/app/app.module.ts
+++ angular-documentation/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -18,7 +19,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
